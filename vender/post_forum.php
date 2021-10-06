0da89dc1751c972dac5fcb8e4fa288e0d9917654
Samim Zubair <bsamimzubair@gmail.com>
2021-10-06 18:20:13 +0500

    post forum page made dynamic

--- vender/post_forum.php
+++ vender/post_forum.php
@@ -6,138 +6,143 @@ $queryToGetActiveQuotes = "SELECT * FROM quotes WHERE effective_date >= '$date'"
 $results = db::getRecords($queryToGetActiveQuotes);
 ?>
 <style>
-.utf-buttons-to-right {
-  opacity: 1;
-}
-.utf-buttons-to-right{
-  top: 30px !important;
-}
+  .utf-buttons-to-right {
+    opacity: 1;
+  }
 
-/*.utf-job-listing.width-adjustment.message_all_wrapper{
+  .utf-buttons-to-right {
+    top: 30px !important;
+  }
+
+  /*.utf-job-listing.width-adjustment.message_all_wrapper{
   display: none;
 }*/
-/*chat sysytem end*/
-  ul.utf-dashboard-box-list > li:hover {
-    
-     transform: unset; 
-}
+  /*chat sysytem end*/
+  ul.utf-dashboard-box-list>li:hover {
 
+    transform: unset;
+  }
 </style>
 
 <!-- Dashboard Content -->
 <div class="utf-dashboard-content-container-aera" data-simplebar>
- <div id="dashboard-titlebar" class="utf-dashboard-headline-item">
-  <div class="row">
-   <div class="col-xl-12">  
-    <h3>Post Forum</h3>
-    <nav id="breadcrumbs">
-     <ul>
-      <li><a href="index.php">Home</a></li>
-      <li>Post Forum</li>
-    </ul>
-  </nav>
-</div>
-</div>    
-</div>  
-<div class="utf-dashboard-content-inner-aera"> 
-  <div class="row"> 
-    <div class="col-xl-12">
-      <div class="dashboard-box margin-top-0"> 
-        <div class="headline">
-          <h3>Posts</h3>
-        </div>
-        <div class="content">
-          <ul class="utf-dashboard-box-list">
-            <li> 
-            <div class="utf-job-listing width-adjustment">
-                  <div class="utf-job-listing-details">
-                    <div class="utf-job-listing-description">
-                      <h3 class="utf-job-listing-title"><a href="#">Commercial Insurance</a></h3>
-                      <div class="utf-job-listing-footer">
-                        <ul>
-                          <li><i class="icon-material-outline-date-range"></i> 12 Jan, 2021 12:30 PM</li>
-                          <li><i class="icon-feather-briefcase"></i> Commercial Insurance</li>
-                          <li><i class="icon-material-outline-location-on"></i> 2767 Concord Street, Charlotte</li>
-                        </ul>
-                        <a href="">Document.pdf</a>
-                        <p>Switching car insurance companies is a great way to help you get the most for your money.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <ul class="dashboard-task-info">
-                  <li><a href="chat.php"><span>Chat</span></a></li>
-                  <li><span>Efected Date</span></li>
-                </ul>
-              <div class="utf-buttons-to-right"> 
-                <!-- <a href="#small-dialog" class="popup-with-zoom-anim button green ripple-effect ico" title="Edit Bids" data-tippy-placement="top"><i class="icon-feather-edit"></i></a> -->
-                <a href="#" class="button red ripple-effect ico" title="Interested" data-tippy-placement="top"><i id="fv1" class="icon-material-outline-favorite-border" onclick="toggle_fv(this.id)"></i></a>
-              </div>
-              
-              
-            </li>
-            <li> 
-            <div class="utf-job-listing width-adjustment">
-                  <div class="utf-job-listing-details">
-                    <div class="utf-job-listing-description">
-                      <h3 class="utf-job-listing-title"><a href="#">Commercial Insurance</a></h3>
-                      <div class="utf-job-listing-footer">
-                        <ul>
-                          <li><i class="icon-material-outline-date-range"></i> 12 Jan, 2021 12:30 PM</li>
-                          <li><i class="icon-feather-briefcase"></i> Commercial Insurance</li>
-                          <li><i class="icon-material-outline-location-on"></i> 2767 Concord Street, Charlotte</li>
-                        </ul>
-                        <a href="">Document.pdf</a>
-                        <p>Switching car insurance companies is a great way to help you get the most for your money.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <ul class="dashboard-task-info">
-                  <li><a href="chat.php"><span>Chat</span></a></li>
-                  <li><span>Efected Date</span></li>
-                </ul>
-              <div class="utf-buttons-to-right"> 
-                <!-- <a href="#small-dialog" class="popup-with-zoom-anim button green ripple-effect ico" title="Edit Bids" data-tippy-placement="top"><i class="icon-feather-edit"></i></a> -->
-                <a href="#" class="button red ripple-effect ico" title="Interested" data-tippy-placement="top"><i id="fv2" class="icon-material-outline-favorite-border" onclick="toggle_fv(this.id)"></i></a> 
-              </div>
-               
-            </li>
-            <li> 
-            <div class="utf-job-listing width-adjustment">
-                  <div class="utf-job-listing-details">
-                    <div class="utf-job-listing-description">
-                      <h3 class="utf-job-listing-title"><a href="#">Commercial Insurance</a></h3>
-                      <div class="utf-job-listing-footer">
-                        <ul>
-                          <li><i class="icon-material-outline-date-range"></i> 12 Jan, 2021 12:30 PM</li>
-                          <li><i class="icon-feather-briefcase"></i> Commercial Insurance</li>
-                          <li><i class="icon-material-outline-location-on"></i> 2767 Concord Street, Charlotte</li>
-                        </ul>
-                        <a href="">Document.pdf</a>
-                        <p>Switching car insurance companies is a great way to help you get the most for your money.</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <ul class="dashboard-task-info">
-                  <li><a href="chat.php"><span>Chat</span></a></li>
-                  <li><span>Efected Date</span></li>
-                </ul>
-             <div class="utf-buttons-to-right"> 
-              <!-- <a href="#small-dialog" class="popup-with-zoom-anim button green ripple-effect ico" title="Edit Bids" data-tippy-placement="top"><i class="icon-feather-edit"></i></a> -->
-              <a href="#" class="button red ripple-effect ico" title="Interested" data-tippy-placement="top"><i id="fv" class="icon-material-outline-favorite-border" onclick="toggle_fv(this.id)"></i></a>
-            </div>
-             
-
-          </li>
-        </ul>
+  <div id="dashboard-titlebar" class="utf-dashboard-headline-item">
+    <div class="row">
+      <div class="col-xl-12">
+        <h3>Post Forum</h3>
+        <nav id="breadcrumbs">
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li>Post Forum</li>
+          </ul>
+        </nav>
       </div>
     </div>
   </div>
-</div>
+  <div class="utf-dashboard-content-inner-aera">
+    <div class="row">
+      <div class="col-xl-12">
+        <div class="dashboard-box margin-top-0">
+          <div class="headline">
+            <h3>Posts</h3>
+          </div>
+          <div class="content">
+            <ul class="utf-dashboard-box-list">
+              <?php
+              if ($results) {
+                foreach ($results as $result) {
+              ?>
+                  <li>
+                    <div class="utf-job-listing width-adjustment">
+                      <div class="utf-job-listing-details">
+                        <div class="utf-job-listing-description">
+                          <h3 class="utf-job-listing-title"><a href="#"><?php echo $result['business_name']; ?></a></h3>
+                          <div class="utf-job-listing-footer">
+                            <ul>
+                              <li><i class="icon-line-awesome-tasks"></i><?php echo $result['insurance_type']; ?></li>
+                              <li><i class="icon-material-outline-date-range"></i><?php echo $result['time_stamp']; ?></li>
+                              <li><i class="icon-feather-briefcase"></i><?php echo $result['business_type']; ?></li>
+                              <li><i class="icon-material-outline-location-on"></i><?php echo $result['city']; ?>, <?php echo $result['state']; ?></li>
+                              <li><i class="icon-line-awesome-dollar"></i><?php echo $result['gross_sales']; ?></li>
+                            </ul>
+                            <a href="files/<?php echo $result['file']; ?>" download>Download Attachment</a>
+                            <p><?php echo $result['description']; ?></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <ul class="dashboard-task-info">
+                      <li><a href="chat.php"><span>Chat</span></a></li>
+                    </ul>
+                    <ul class="dashboard-task-info">
+                      <li><span><b>Effective Date: </b><?php echo $result['effective_date']; ?></span></li>
+                    </ul>
+                    <div class="utf-buttons-to-right">
+                      <a href="#" class="button red ripple-effect ico" title="Interested" data-tippy-placement="top"><i id="fv1" class="icon-material-outline-favorite-border" onclick="toggle_fv(this.id)"></i></a>
+                    </div>
+                  </li>
+              <?php
+                }
+              }
+              ?>
+              <!-- <li>
+                <div class="utf-job-listing width-adjustment">
+                  <div class="utf-job-listing-details">
+                    <div class="utf-job-listing-description">
+                      <h3 class="utf-job-listing-title"><a href="#">Commercial Insurance</a></h3>
+                      <div class="utf-job-listing-footer">
+                        <ul>
+                          <li><i class="icon-material-outline-date-range"></i> 12 Jan, 2021 12:30 PM</li>
+                          <li><i class="icon-feather-briefcase"></i> Commercial Insurance</li>
+                          <li><i class="icon-material-outline-location-on"></i> 2767 Concord Street, Charlotte</li>
+                        </ul>
+                        <a href="">Document.pdf</a>
+                        <p>Switching car insurance companies is a great way to help you get the most for your money.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <ul class="dashboard-task-info">
+                  <li><a href="chat.php"><span>Chat</span></a></li>
+                  <li><span>Efected Date</span></li>
+                </ul>
+                <div class="utf-buttons-to-right">
+                  <a href="#" class="button red ripple-effect ico" title="Interested" data-tippy-placement="top"><i id="fv2" class="icon-material-outline-favorite-border" onclick="toggle_fv(this.id)"></i></a>
+                </div>
 
-<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+              </li>
+              <li>
+                <div class="utf-job-listing width-adjustment">
+                  <div class="utf-job-listing-details">
+                    <div class="utf-job-listing-description">
+                      <h3 class="utf-job-listing-title"><a href="#">Commercial Insurance</a></h3>
+                      <div class="utf-job-listing-footer">
+                        <ul>
+                          <li><i class="icon-material-outline-date-range"></i> 12 Jan, 2021 12:30 PM</li>
+                          <li><i class="icon-feather-briefcase"></i> Commercial Insurance</li>
+                          <li><i class="icon-material-outline-location-on"></i> 2767 Concord Street, Charlotte</li>
+                        </ul>
+                        <a href="">Document.pdf</a>
+                        <p>Switching car insurance companies is a great way to help you get the most for your money.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <ul class="dashboard-task-info">
+                  <li><a href="chat.php"><span>Chat</span></a></li>
+                  <li><span>Efected Date</span></li>
+                </ul>
+                <div class="utf-buttons-to-right">
+                  <a href="#" class="button red ripple-effect ico" title="Interested" data-tippy-placement="top"><i id="fv" class="icon-material-outline-favorite-border" onclick="toggle_fv(this.id)"></i></a>
+                </div>
+              </li> -->
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript">
 
 
@@ -171,12 +176,12 @@ $results = db::getRecords($queryToGetActiveQuotes);
     //   }
     // } 
   </script> -->
-  <script>
-    function toggle_fv(clicked_id) {
-      var NAME = document.getElementById(clicked_id)
-      NAME.classList.toggle("icon-material-outline-favorite");
-    }
-  </script>
-  <?php
-  require_once("dashboard_footer.php");
-?>
+    <script>
+      function toggle_fv(clicked_id) {
+        var NAME = document.getElementById(clicked_id)
+        NAME.classList.toggle("icon-material-outline-favorite");
+      }
+    </script>
+    <?php
+    require_once("dashboard_footer.php");
+    ?>
